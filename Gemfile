831eec6ee41da420c93628b9635517c2c3e1bcd0
Shoshana Weinstock-Herman <sjweinstock.herman@gmail.com>
2019-05-29 10:23:36 -0400

    Adding carrierwave support

--- Gemfile
+++ Gemfile
@@ -68,4 +68,7 @@ end
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "faker"
+gem "carrierwave"
+gem "fog"
+gem "figaro"
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+# config/initiliazers/carrierwave.rb
+
+CarrierWave.configure do |config|
+config.fog_credentials = {
+  :provider              => 'AWS',
+  :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
+  :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+  :region                => "#{ENV['S3_REGION']}",
+  :path_style            => true,
+:host => "s3-#{ENV["S3_REGION"]}.amazonaws.com",
+:endpoint => "https://s3-#{ENV["S3_REGION"]}.amazonaws.com",
+}
+
+config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
+config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+end
